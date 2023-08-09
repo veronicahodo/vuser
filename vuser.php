f8d3cd7c1d06944ed1ac21249368783d02810daf
Veronica Hodo <90301991+veronicahodo@users.noreply.github.com>
2023-08-08 19:24:37 -0700

    Update vuser.php
    More or less completed initial draft for this class

--- vuser.php
+++ vuser.php
@@ -14,6 +14,10 @@ class VUSER {
 
 
   function __construct() {
+    $this->userId = 0;
+    $this->username = '';
+    $this->hash = '';
+    $this->salt = '';
 
   }
 
@@ -40,17 +44,37 @@ class VUSER {
     }
   }
 
-  function save() {
+  function save(VCRUD $c) {
+    if ($this->userId > 0) {
+      $c->create('users',[
+                 'username' => $this->username,
+                 'hash' => $this->hash,
+                 'salt' => $this->salt
+                 ]);
+    } else {
+      $c->update('users',[
+                 'username'=>$this->username,
+                 'hash'=>$this->hash,
+                 'salt'=>$this->salt
+                 ],[
+                 ['userId','=',$this->userId]
+                 ]);
 
+    }
   }
 
 
-  function getUserData($userId) {
-
-    
+  function getUserData($userId, VCRUD $c) {
+    $data = $c->read('userdata',[['userId','=',$userId]]);
+    // lol so not failsafe [TODO]
+    return $data[0];    
   }
 
   function validatePassword($username,$password, VCRUD $c) {
+    // I'm conflicted on leaving this to where it doesn't
+    // modify our internal user. I don't know why we would ever NOT
+    // want current user data in this situation but ... why waste cycles?
+    // I'm obviously open to suggestions
     $user = $c->read('users',[['username','=',$username]]);
     if ($user) {
       if (hash('sha512',$password.$user[0]['salt'])) {
