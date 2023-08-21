7ec60399ab3fef17cff5ff1f28221d531876e9b3
Veronica <veronica@veronicahodo.com>
2023-08-20 20:23:22 -0700

    Added curr ver vcrud.php

---
+++ vcrud.php
@@ -0,0 +1,104 @@
+<?php
+
+// vcrud.php
+
+// Basic class for handing CRUD database interactions.  Designed to
+// be extended for whatever the intended use would be
+
+// Version 1.0.0
+
+class VCRUD
+{
+	private $connection;	// Stores the PDO connection so we don't have to pass it every time
+	private $maxRows = 20000;
+
+	public function __construct($dbUser, $dbPass, $dbHost, $dbName)
+	{
+		// Just calls the connect function. Did it this way in case I ever wanted to change
+		// databases
+		$this->connect($dbUser, $dbPass, $dbHost, $dbName);
+	}
+
+	private function connect($dbUser, $dbPass, $dbHost, $dbName)
+	{
+		// connects to a mysql/mariadb database
+		$dsn = "mysql:host={$dbHost};dbname={$dbName}";
+		$this->connection  = new PDO($dsn, $dbUser, $dbPass);
+	}
+
+	private function conditionsToStrings($conditions)
+	{
+		// turns the datasets [column,operand,value] into the SQL string formatted
+		// markup
+		$working = [];
+		foreach ($conditions as $condition) {
+			[$column, $operator, $value] = $condition;
+			$workingStr = "{$column} {$operator} ";
+			// if it's a LIKE operand we have to add the % to either side
+			if (strtolower($operator) === 'like') {
+				$workingStr .= "%{$value}%";
+			} else {
+				$workingStr .= $value;
+			}
+			$working[] = $workingStr;
+		}
+		return $working;
+	}
+
+	public function create($table, $fields)
+	{
+		// inserts a single row into the database. Perhaps in the future it will
+		// support multi row but for now until I need it, this is fine
+		$columns = array_keys($fields);
+		$placeholders = ':' . implode(',:', $columns);
+		$sql = "INSERT INTO `{$table}` (" . implode(',', $columns) . ") VALUES ({$placeholders})";
+		$stmt = $this->connection->prepare($sql);
+		$stmt->execute($fields);
+		return $this->connection->lastInsertId();
+	}
+
+	public function read($table, $conditions)
+	{
+		// reads up to 20000 rows and returns them based on conditions. 
+		// Conditions are formatted [column,operand,value]
+		$strConditions = $this->conditionsToStrings($conditions);
+		$sql = "SELECT * FROM `{$table}` WHERE (" . implode(' AND ', $strConditions) . ") LIMIT " . $this->maxRows;
+		$stmt = $this->connection->prepare($sql);
+		$stmt->execute();
+		$return = [];
+		while ($line = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			$return[] = $line;
+		}
+		return $return;
+	}
+
+	public function update($table, $fields, $conditions)
+	{
+		// upates up to 20000 rows with the data from fields.
+		// Conditions are formated based on [column,operand,value]
+		$strConditions = $this->conditionsToStrings($conditions);
+		$frames = [];
+		foreach (array_keys($fields) as $column) {
+			$frames[] = "{$column}=:{$column}";
+		}
+		$sql = "UPDATE `{$table}` SET " . implode(',', $frames) . " WHERE (" . implode(' AND ', $strConditions) . ") LIMIT " . $this->maxRows;
+		$stmt = $this->connection->prepare($sql);
+		$stmt->execute($fields);
+	}
+
+	public function delete($table, $conditions)
+	{
+		// deletes up to 20000 rows that meet the conditions
+		// Conditions are formatted based on [colum,operand,value]
+		$strConditions = $this->conditionsToStrings($conditions);
+		$sql = "DELETE FROM `{$table}` WHERE (" . implode(' AND ', $strConditions) . ") LIMIT " . $this->maxRows;
+		$stmt = $this->connection->prepare($sql);
+		$stmt->execute();
+	}
+
+	public function close()
+	{
+		// as for good cleanup,this should be called before exiting
+		$this->connection = null;
+	}
+}
--- vuser.php
+++ vuser.php
@@ -6,88 +6,89 @@
 
 require_once('vcrud.php');
 
-class VUSER {
-  private $userId;
-  private $username;
-  private $hash;
-  private $salt;
+class VUSER
+{
+    private $userId;
+    private $username;
+    private $hash;      // sha512 hash of plain text password + salt
+    private $salt;
 
 
-  function __construct() {
-    $this->userId = 0;
-    $this->username = '';
-    $this->hash = '';
-    $this->salt = '';
-
-  }
-
-
-  function create($username,$password, VCRUD $c) {
-    $this->salt = random_bytes(32);
-    $c->create('users',[
-               'username'=>$username,
-               'hash'=>hash('sha512',$password.$this->salt),
-               'salt' => $this->salt
-               ]);
-  }
-
-  function load($userId, VCRUD $c) {
-    $data = $c->read('users',[['userId','=',$userId]]);
-    if ($data) {
-      $this->userId = $data[0]['userId'];
-      $this->username = $data[0]['username'];
-      $this->hash = $data[0]['hash'];
-      $this->salt = $data[0]['salt'];
-      return $data[0];
-    } else {
-      return false;
+    function __construct()
+    {
+        $this->userId = 0;
+        $this->username = '';
+        $this->hash = '';
+        $this->salt = '';
     }
-  }
 
-  function save(VCRUD $c) {
-    if ($this->userId > 0) {
-      $c->create('users',[
-                 'username' => $this->username,
-                 'hash' => $this->hash,
-                 'salt' => $this->salt
-                 ]);
-    } else {
-      $c->update('users',[
-                 'username'=>$this->username,
-                 'hash'=>$this->hash,
-                 'salt'=>$this->salt
-                 ],[
-                 ['userId','=',$this->userId]
-                 ]);
 
+    function create($username, $password, VCRUD $c)
+    {
+        $this->salt = random_bytes(32);
+        $c->create('users', [
+            'username' => $username,
+            'hash' => hash('sha512', $password . $this->salt),
+            'salt' => $this->salt
+        ]);
     }
-  }
 
-
-  function getUserData($userId, VCRUD $c) {
-    $data = $c->read('userdata',[['userId','=',$userId]]);
-    // lol so not failsafe [TODO]
-    return $data[0];    
-  }
-
-  function validatePassword($username,$password, VCRUD $c) {
-    // I'm conflicted on leaving this to where it doesn't
-    // modify our internal user. I don't know why we would ever NOT
-    // want current user data in this situation but ... why waste cycles?
-    // I'm obviously open to suggestions
-    $user = $c->read('users',[['username','=',$username]]);
-    if ($user) {
-      if (hash('sha512',$password.$user[0]['salt'])) {
-        return $user[0]['userId'];
-      } else {
-        return false;
-      }
-    } else {
-      return false;
+    function load($userId, VCRUD $c)
+    {
+        $data = $c->read('users', [['userId', '=', $userId]]);
+        if ($data) {
+            $this->userId = $data[0]['userId'];
+            $this->username = $data[0]['username'];
+            $this->hash = $data[0]['hash'];
+            $this->salt = $data[0]['salt'];
+            return $data[0];
+        } else {
+            return false;
+        }
     }
-  }
+
+    function save(VCRUD $c)
+    {
+        if ($this->userId > 0) {
+            $c->create('users', [
+                'username' => $this->username,
+                'hash' => $this->hash,
+                'salt' => $this->salt
+            ]);
+        } else {
+            $c->update('users', [
+                'username' => $this->username,
+                'hash' => $this->hash,
+                'salt' => $this->salt
+            ], [
+                ['userId', '=', $this->userId]
+            ]);
+        }
+    }
+
+
+    function getUserData($userId, VCRUD $c)
+    {
+        $data = $c->read('userdata', [['userId', '=', $userId]]);
+        // lol so not failsafe [TODO]
+        return $data[0];
+    }
+
+    function validatePassword($username, $password, VCRUD $c)
+    {
+        // I'm conflicted on leaving this to where it doesn't
+        // modify our internal user. I don't know why we would ever NOT
+        // want current user data in this situation but ... why waste cycles?
+        // I'm obviously open to suggestions
+        $user = $c->read('users', [['username', '=', $username]]);
+        if ($user) {
+            if (hash('sha512', $password . $user[0]['salt'])) {
+                return $user[0]['userId'];
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
-
-
-
-
